54456ad7ae6cd4d4398e84c2dbcfd7c73a197c80
Kyler Rynear <kyler.rynear@gmail.com>
2014-08-10 22:15:42 -0700

    fixed capatilization of commandLine on line 36

--- src/ls.cpp
+++ src/ls.cpp
@@ -33,7 +33,7 @@ int main() {
 	}
 	
 	if (directories.size() > 0) {
-		char* commandLine = new char[(commandline.size() + 1) * sizeof(char*)];
+		char* commandLine = new char[(commandLine.size() + 1) * sizeof(char*)];
 		strcpy(commandLine, commandInput.c_str());
 
 		char const *dirName = ;
