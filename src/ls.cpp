9156a8541dba3dc582428429c5277bf5f5bf6e5b
Kyler Rynear <kyler.rynear@gmail.com>
2014-08-12 02:32:11 -0700

    changed conditions for boolean variables in lswithflags function

--- src/ls.cpp
+++ src/ls.cpp
@@ -136,7 +136,7 @@ int main(int argc, char* argv[]) {
 				}
 			}	
 
-			for (unsigned int i = 0; i < directories.size(); ++i) {
+			for (int i = 0; i < directories.size(); ++i) {
 				flags.clear();
 				for (unsigned int k = static_cast<unsigned>(directoryIndex.at(i)); 
 					 (unsigned)i + 1 < directoryIndex.size() && k != directoryIndex.at(i + 1);
