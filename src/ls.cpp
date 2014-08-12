2762ba1873ff24b8652e1d95f61a00ea59ad220b
Kyler Rynear <kyler.rynear@gmail.com>
2014-08-12 02:18:33 -0700

    changed conditions for boolean variables in lswithflags function

--- src/ls.cpp
+++ src/ls.cpp
@@ -146,7 +146,7 @@ int main(int argc, char* argv[]) {
 					}
 				}
 
-				if (errno == lsWithFlags(directories.at(i), flags) {
+				if (errno == lsWithFlags(directories.at(i), flags)) {
 				
 					return errno;
 				}
