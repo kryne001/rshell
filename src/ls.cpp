465693fee7feca465eb7e6ad7a4014cdbb929369
Kyler Rynear <kyler.rynear@gmail.com>
2014-08-11 23:45:29 -0700

    added command line arguments, should work for basic ls

--- src/ls.cpp
+++ src/ls.cpp
@@ -6,49 +6,29 @@
 #include <sys/types.h>
 using namespace std;
 
-int main() {
+int main(int argc, char** argv[]) {
 	
-	char host[128];
-	gethostname(host, sizeof host);
-	string commandInput;
-
-	cout << getlogin() << "@" << host << "$ ";
-	cin >> commandInput;
-	cout << endl;
-
-	istringstream commandStream(commandInput);
-	string currentCommand;
-	vector<string> flags;
-	vector<string> directories;
-
-	unsigned i;
-	while (commandStream >> currentCommand) {
-		if (currentCommand.at(0) == '-') {
-
-			flags.push_back(currentCommand);		
-		}
-		else if (currentCommand != "ls") {
-			directories.push_back(currentCommand);
-		}
-	}
-	
-	if (directories.size() > 0) {
-		char* commandLine = new char[(commandLine.size() + 1) * sizeof(char*)];
-		strcpy(commandLine, commandInput.c_str());
-
-		char const *dirName = ;
+	if (argc == 1) {
+		
+		char const *dirName = ".";
 		DIR *dirp;
 		if (!(dirp = opendir(dirName))) {
-			cerr << "Error(" << errno << ") opening " << dirName << endl;
+		cerr << "Error(" << errno << ") opening " << dirName << endl;
 			return errno;
 		}
 
 		dirent *direntp;
-		while ((direntp = readdir(dirp)))
-			cout << direntp->d_name << endl; // use stat here to find attributes of a file
-	
+		while ((direntp = readdir(dirp))) {
+			if (direntp->d_name[0] != '.') {
+					
+				cout << direntp->d_name << endl; // use stat here to find attributes of a file
+			}
+		}
 		closedir(dirp);
-	}
+	}	
+	
+
+
 }
 
 
