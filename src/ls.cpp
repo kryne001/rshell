19c94ade5780fea09c851f27d4eac8c4a5bea27c
Kyler Rynear <kyler.rynear@gmail.com>
2014-08-12 08:33:54 -0700

    added -l protection functionality

--- src/ls.cpp
+++ src/ls.cpp
@@ -218,7 +218,7 @@ int main(int argc, char* argv[]) {
 			} else {
 			
 				directories.push_back(argv[i]);
-				directories.at(i).insert(0, toInsert);
+				//directories.at(i).insert(0, toInsert);
 			}
 		}
 		if (flags.size() == 0) {
